b0df6da7b11ab139627ada4ad67ddd9cfcd06d3c
Abdalla Sufi <abdallasufi88@gmail.com>
2022-06-16 12:37:17 +0600

    Update variables.tf

--- Infrastructure as Code/terraform/variables.tf
+++ Infrastructure as Code/terraform/variables.tf
@@ -1,20 +1,6 @@
 variable "resource_group_name" {
   type = string
 }
-variable "tenant_id" {
-  type = string
-}
-
-variable "subscription_id" {
-  type = string
-}
-
-variable "client_id" {
-  type = string
-}
-variable "client_secret" {
-  type = string
-}
 variable "location" {
   type = string
 
@@ -24,7 +10,27 @@ variable "vm_size" {
   type    = string
   default = "Standard_B2_s"
 }
+
 variable "db_location" {
   type = string
   default = "eastasia"
 }
+
+# #########################################################################
+# # Terraform gets these values from ENV variables set during execution.###
+# # This variables is not needed when azure executes pipline            ###
+# #########################################################################
+# variable "tenant_id" {
+#   type = string
+# }
+
+# variable "subscription_id" {
+#   type = string
+# }
+
+# variable "client_id" {
+#   type = string
+# }
+# variable "client_secret" {
+#   type = string
+# }
